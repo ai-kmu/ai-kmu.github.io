ede475fd0992f473cfb833d22e2bd32224431db8
lhj-3 <dlrhkswhd3381@gmail.com>
2021-09-01 16:01:02 +0900

    add inter tag

--- js/scripts.js
+++ js/scripts.js
@@ -90,7 +90,15 @@
                                     html_.push(data_.card[i].subcard[j].contents[k].description[l].area + '<br>')
                                 }
                             }
-                            html_.push('</small></p></div>');
+                            html_.push('</small>');
+                            if (data_.card[i].subcard[j].contents[k].inter !== "") { // 인턴인 경우
+                                html_.push("<strong>Inter at")
+                                html_.push('<br></strong><small>'+data_.card[i].subcard[j].contents[k].inter+'</small>')
+                            }
+                            else if (data_.card[i].subcard[j].position === "Graduate Students") {
+                                html_.push('<br><br>')
+                            }
+                            html_.push('</p></div>');
                             if (data_.card[i].subcard[j].contents[k].email !== "") { // email이 필요한 경유
                                 html_.push('<div class="card-footer">');
                                 html_.push('<div class="row align-items-center">');
